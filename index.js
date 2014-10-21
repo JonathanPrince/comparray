5f587adb9f82181502c0dbbf0aa4c5d4bf49136f
Jonathan Prince <jonathan.prince@gmail.com>
2014-10-21 13:23:38 +0200

    update module to recursively compare arrays

--- index.js
+++ index.js
@@ -2,35 +2,39 @@
 
 var assert = require('assert');
 
-module.exports = function(arr1, arr2){
+module.exports = function(array1, array2){
 
-  assert.equal(arr1 instanceof Array, true, 'first arguement should be an array');
-  assert.equal(arr2 instanceof Array, true, 'second arguement should be an array');
+  assert.equal(array1 instanceof Array, true, 'first arguement should be an array');
+  assert.equal(array2 instanceof Array, true, 'second arguement should be an array');
 
-  if(arr1.length !== arr2.length) {
+  if(array1.length !== array2.length) {
     return false;
   }
 
+  function compareArrayElements(firstArray, secondArray){
 
-  for(var i = 0; i < arr1.length; i++) {
+    for(var i = 0; i < firstArray.length; i++) {
 
-    if (arr1[i] !== arr2[i]) {
+      if (firstArray[i] !== secondArray[i]) {
 
-      // check if element is itself an array
-      if (arr1[i] instanceof Array && arr1[i] instanceof Array) {
+        // check if element is itself an array
+        if (firstArray[i] instanceof Array && secondArray[i] instanceof Array) {
 
-        for (var j = 0; j < arr1[i].length; j++){
+          compareArrayElements(firstArray[i], secondArray[i]);
 
-          if (arr1[i][j] !== arr2[i][j]) return false;
+        } else {
 
+          return false;
         }
 
-      } else {
-
-        return false;
       }
     }
+
+    return true;
+
   }
 
-  return true;
+
+  return compareArrayElements(array1, array2);
+
 };
